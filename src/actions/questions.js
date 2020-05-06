19309cc2c5f4d5fbc6c7f9aaeb9a57b4e33849d1
Adrian-Baghdasarian <adbaghdasarian@gmail.com>
2020-05-05 20:24:54 -0700

    created and added functionality to New Question page.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,44 @@
+import { _saveQuestion as saveQuestion } from '../utils/_DATA.js'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const ANSWER_QUESTION = 'ANSWER_QUESTION';
+export const ADD_QUESTION = 'ADD_QUESTION';
+
 
 export function receiveQuestions (questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions,
+  }
+}
+
+
+export function answerQuestion({ id, uthedUser, choice}){
+
+  return;
+}
+
+function addQuestion (question) {
+  return {
+    type: ADD_QUESTION,
+    question
+  }
+}
+
+export function handleAddQuestion (text1, text2) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+
+    dispatch(showLoading());
+    //console.log(text1, text2);
+
+    return saveQuestion({
+      optionOneText: text1,
+      optionTwoText: text2,
+      author: authedUser,
+    })
+      .then((question) => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading()))
   }
 }
--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,15 +2,83 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
+import {handleAddQuestion} from '../actions/questions';
+
 class NewQuestion extends Component {
+  state = {
+    text1: '',
+    text2: ''
+  }
+
+  handleChange1 = (e) => {
+    const text1 = e.target.value
+
+    this.setState(() => ({
+      text1
+    }))
+  }
+
+  handleChange2 = (e) => {
+    const text2 = e.target.value
+
+    this.setState(() => ({
+      text2
+    }))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { text1, text2 } = this.state;
+    const { dispatch } = this.props;
+
+    console.log(
+      'New Question: ', text1, 'or', text2
+      );
+    dispatch(handleAddQuestion(text1, text2));
 
 
+    this.setState(() => ({
+      text1: '',
+      text2: ''
+    }))
+  }
   render() {
+    const { text1, text2 } = this.state
 
     return (
 
-      <div>
-        NewQuestion
+      <div id='new-question-container'>
+        <h3 className='center create-new-question-header'> Create New Question</h3>
+        <br/>
+        <h1> Complete the Question:</h1>
+        <br />
+        <h2 style={{fontWeight: 'bold'}}> Would you rather ...</h2>
+        <form className='new-question' onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="Enter Option One Text Here"
+            value={text1}
+            onChange={this.handleChange1}
+            className='textarea'
+            maxLength={280}
+          />
+          <span style={{fontWeight: 'bold'}}> OR </span>
+          <textarea
+            placeholder="Enter Option One Text Here"
+            value={text2}
+            onChange={this.handleChange2}
+            className='textarea'
+            maxLength={280}
+          />
+          <br/>
+          <button
+            className='btn'
+            type='submit'
+            disabled={text1 === text2 === ''}>
+              Submit
+          </button>
+
+        </form>
       </div>
     )
   }
